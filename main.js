3843970e5d212005f03debfdb24c2ea966178ccf
Maria Kodi <mariakolodii@mac.speedport.ip>
2026-04-22 00:24:44 +0200

    Fetch Pokémon details for each Pokémon

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
   data.results.forEach(pokemon => {
    console.log(pokemon.name);
    console.log(pokemon.url);
+
+   fetch(pokemon.url)
+   .then(res => res.json())
+   .then(details => {
+     console.log(details);
+   });
   });
 })
 .catch(error => console.log(error));
