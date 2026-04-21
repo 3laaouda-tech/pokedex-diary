0d81bbc413af77361db22d2c4dcdca53a7fcd2c0
Maria Kodi <mariakolodii@mac.speedport.ip>
2026-04-22 00:07:26 +0200

    Iterate over Pokémon list and log name and URL

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
 .then(res => res.json())
 .then(data => {
   console.log(data);
-});
+
+  data.results.forEach(pokemon => {
+   console.log(pokemon.name);
+   console.log(pokemon.url);
+  });
+})
+.catch(error => console.log(error));
