cbd6119ac594b82b840d12eddf2a1af4cbd4c8b9
Maria Kodi <mariakolodii@mac.speedport.ip>
2026-04-22 17:50:37 +0200

    Improve Pokémon data mapping and display

--- main.js
+++ main.js
@@ -3,16 +3,42 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
   .then(res => res.json())
   .then(data => {
     return Promise.all(
-      data.results.map(pokemon => fetch(pokemon.url).then(res => res.json()))
+      data.results.map(pokemon =>
+        fetch(pokemon.url).then(res => res.json())
+      )
     );
   })
   .then(pokemons => {
+
+    const PokeDetailsInfo = pokemons.map(details => {
+      return {
+        name: details.name,
+        id: details.id,
+        height: details.height,
+        weight: details.weight,
+        abilities: details.abilities.map(a => a.ability.name),
+        stats: {
+          hp: details.stats[0].base_stat,
+          attack: details.stats[1].base_stat,
+          defense: details.stats[2].base_stat,
+          specialAttack: details.stats[3].base_stat,
+          specialDefense: details.stats[4].base_stat,
+          speed: details.stats[5].base_stat,
+        },
+        types: details.types.map(t => t.type.name),
+        sprite: details.sprites.front_default,
+        notes: "",
+      };
+    });
+
+    console.log(PokeDetailsInfo);
+
     const grid = document.querySelector(".dex-grid");
     grid.innerHTML = "";
 
-    pokemons.forEach(details => {
-      const types = details.types
-        .map(t => `<span class="type-badge type-${t.type.name}">${t.type.name}</span>`)
+    PokeDetailsInfo.forEach(pokemon => {
+      const types = pokemon.types
+        .map(t => `<span class="type-badge type-${t}">${t}</span>`)
         .join("");
 
       const card = document.createElement("a");
@@ -21,11 +47,11 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
 
       card.innerHTML = `
         <div class="dex-card__img">
-          <img src="${details.sprites.front_default}" alt="${details.name}">
+          <img src="${pokemon.sprite}" alt="${pokemon.name}">
         </div>
         <div class="dex-card__body">
-          <div class="dex-card__number">#${details.id}</div>
-          <div class="dex-card__name">${details.name}</div>
+          <div class="dex-card__number">#${pokemon.id}</div>
+          <div class="dex-card__name">${pokemon.name}</div>
           <div class="flex gap-1">${types}</div>
         </div>`;
 
