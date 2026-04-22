6571c34622a93c718541b3b0087653fe8b2f25d1
Maria Kodi <mariakolodii@mac.speedport.ip>
2026-04-22 12:39:52 +0200

    Add Pokémon cards with API data (name, image, stats)

--- main.js
+++ main.js
@@ -13,12 +13,20 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
    .then(details => {
     const name = details.name;
     const image = details.sprites.front_default;
+    const hitPoints = details.stats[0].base_stat;
+    const attack = details.stats[1].base_stat;
+    const defense = details.stats[2].base_stat;
+
     const div = document.createElement("div");
 
     div.innerHTML = `<h2>${name}</h2>
-    <img src="${image}" alt="${name}">`;
+    <img src="${image}" alt="${name}">
+    <p>Hit Point: ${hitPoints}</p>
+    <p>Attack: ${attack}</p>
+    <p>Defense: ${defense}</p>
+    `;
     document.body.appendChild(div);
-    
+
      console.log("Name:", details.name);
      console.log("Image:", details.sprites.front_default);
    })
