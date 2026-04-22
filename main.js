d16790336f586d880c38ddf3dc33a4f90166bf39
Maria Kodi <mariakolodii@mac.speedport.ip>
2026-04-22 12:03:59 +0200

    Display Pokémon name and image on the page

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
+// Maria //
 fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
 .then(res => res.json())
 .then(data => {
   console.log(data);
 
   data.results.forEach(pokemon => {
-   console.log(pokemon.name);
-   console.log(pokemon.url);
+   console.log("Name:", pokemon.name);
+   console.log("URL:", pokemon.url);
 
    fetch(pokemon.url)
    .then(res => res.json())
    .then(details => {
-     console.log(details);
-   });
+    const name = details.name;
+    const image = details.sprites.front_default;
+    const div = document.createElement("div");
+
+    div.innerHTML = `<h2>${name}</h2>
+    <img src="${image}" alt="${name}">`;
+    document.body.appendChild(div);
+    
+     console.log("Name:", details.name);
+     console.log("Image:", details.sprites.front_default);
+   })
+   .catch(error => console.error(error));
   });
 })
-.catch(error => console.log(error));
+.catch(error => console.error(error)); 
